1dc67b8ab9291076c8e08eb97e5bee3dc80f327f
DHARNEESH S L <dharaneesh19112006@gmail.com>
2026-07-04 14:36:47 +0530

    Time: 56 ms (38.14%), Space: 19.7 MB (76.71%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,17 +1,19 @@
 class Solution:
-    def shortestPalindrome(self, s: str) -> str:
-        r = s[::-1]
-        t = s + "#" + r
-
-        lps = [0] * len(t)
-        j = 0
-
-        for i in range(1, len(t)):
-            while j > 0 and t[i] != t[j]:
-                j = lps[j - 1]
-            if t[i] == t[j]:
-                j += 1
-                lps[i] = j
-
-        k = lps[-1]
-        return r[:len(s) - k] + s
+    def shortestPalindrome(self,s:str)->str:
+        if not s:
+            return s
+        base=131
+        mod=10**9+7
+        n=len(s)
+        r=s[::-1]
+        pre=0
+        rev=0
+        power=1
+        best=0
+        for i in range(n):
+            pre=(pre*base+ord(s[i]))%mod
+            rev=(rev+ord(s[i])*power)%mod
+            if pre==rev:
+                best=i+1
+            power=(power*base)%mod
+        return r[:n-best]+s
