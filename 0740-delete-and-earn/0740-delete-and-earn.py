160a929e2276ae2559f4bbe6a9bff1d28d203260
DHARNEESH S L <dharaneesh19112006@gmail.com>
2026-07-14 23:21:38 +0530

    Time: 3 ms (92.4%), Space: 20.5 MB (81.57%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,15 @@
+class Solution:
+    def deleteAndEarn(self, nums: list[int]) -> int:
+        max_num=max(nums)
+        points=[0]*(max_num + 1)
+        for num in nums:
+            points[num]+=num
+        prev2=0
+        prev1=0
+
+        for value in points:
+            curr=max(prev1, value + prev2)
+            prev2=prev1
+            prev1=curr
+
+        return prev1
