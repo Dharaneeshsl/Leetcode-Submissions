25f7f910ea4812ceb3fee9fde76193fa483d1875
DHARNEESH S L <dharaneesh19112006@gmail.com>
2026-07-25 21:28:24 +0530

    Time: 66 ms (45.78%), Space: 21.9 MB (99.44%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,16 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
+        ans=[]
+        prefix=""
+        for ch in searchWord:
+            prefix+=ch
+            temp=[]
+            for word in products:
+                if word.startswith(prefix):
+                    temp.append(word)
+                    if len(temp)==3:
+                        break
+            ans.append(temp)
+
+        return ans
