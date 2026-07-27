7a67ea37b6e13141d4db37072e21e29becaab012
DHARNEESH S L <dharaneesh19112006@gmail.com>
2026-07-27 20:16:49 +0530

    Time: 0 ms (100%), Space: 19.4 MB (44.44%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,24 @@
+class Solution:
+    def myAtoi(self,s:str)->int:
+        i=0
+        n=len(s)
+        while i<n and s[i]==' ':
+            i+=1
+        if i==n:
+            return 0
+        sign=1
+        if s[i]=='+':
+            i+=1
+        elif s[i]=='-':
+            sign=-1
+            i+=1
+        num=0
+        while i<n and s[i].isdigit():
+            num=num*10+ord(s[i])-ord('0')
+            i+=1
+        num*=sign
+        if num<-2**31:
+            return -2**31
+        if num>2**31-1:
+            return 2**31-1
+        return num
